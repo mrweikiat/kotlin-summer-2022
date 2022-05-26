59527ae2fcbf0ffe1df092a9f70fdf7906d24e9d
WEIKIAT-DESKTOP\Admin <danielgohweikiat@yahoo.com>
2022-05-26 20:34:15 +0800

    add permission feature for drawing app

--- DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.drawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +9,9 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -14,6 +19,29 @@ class MainActivity : AppCompatActivity() {
 
     private var drawView: DrawView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){
+            permissions -> permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+
+            if (isGranted){
+                Toast.makeText(
+                    this@MainActivity,
+                    "Permission granted", Toast.LENGTH_LONG
+                ).show()
+            } else {
+                if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission denied", Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +57,14 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.getDrawable(this, R.drawable.selected_pallet)
         )
 
-        val ib_brush: ImageButton = findViewById(R.id.ib_brush)
-        ib_brush.setOnClickListener{
+        val ibBrush: ImageButton = findViewById(R.id.ib_brush)
+        ibBrush.setOnClickListener{
             showBrushSizeChooserDialog()
+        }
+
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener{
+            requestStoragePermission()
         }
     }
 
@@ -74,6 +107,32 @@ class MainActivity : AppCompatActivity() {
             )
             mImageButtonCurrentPaint = view
         }
+    }
+
+    private fun requestStoragePermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            showRationaleDialog("Drawing App", "Drawing App "
+                    + "needs to access your storage")
+            }else {
+                requestPermission.launch(arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                // TODO - Add writing external storage permission
+                ))
+        }
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel") {
+                dialog, _ -> dialog.dismiss()
+        }
+        builder.create().show()
     }
 
 }
