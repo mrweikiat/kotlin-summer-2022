c784f29b48748abc53a0867a84b9ed46d1b78b27
WEIKIAT-DESKTOP\Admin <danielgohweikiat@yahoo.com>
2022-05-27 17:36:42 +0800

    add undo and redo feature for drawing app

--- DrawingApp/app/src/main/java/com/example/drawingapp/DrawView.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/DrawView.kt
@@ -18,9 +18,27 @@ class DrawView(context: Context, attributes: AttributeSet): View(context, attrib
     private var color = Color.BLACK
     private var canvas: Canvas? = null
     private val mPaths = ArrayList<CustomPath>()
+    private val mUndoPaths = ArrayList<CustomPath>()
 
     init{
         setUpDrawing()
+    }
+
+    fun onClickUndo() {
+        if(mPaths.size > 0) {
+            mUndoPaths.add(
+                mPaths.removeAt(mPaths.size - 1))
+            invalidate()
+        }
+    }
+
+    fun onClickRedo() {
+        if (mUndoPaths.size > 0){
+            mPaths.add(
+                mUndoPaths.removeAt(mUndoPaths.size - 1)
+            )
+            invalidate()
+        }
     }
 
     private fun setUpDrawing() {
--- DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.drawingapp
 import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -19,6 +22,17 @@ class MainActivity : AppCompatActivity() {
 
     private var drawView: DrawView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val imageBackground:ImageView = findViewById(R.id.iv_background)
+                imageBackground.setImageURI(result.data?.data)
+            }
+        }
+
     private val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
@@ -28,10 +42,12 @@ class MainActivity : AppCompatActivity() {
                 val isGranted = it.value
 
             if (isGranted){
-                Toast.makeText(
-                    this@MainActivity,
-                    "Permission granted", Toast.LENGTH_LONG
-                ).show()
+                val pickIntent = Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                openGalleryLauncher.launch(pickIntent)
+
+
             } else {
                 if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
                     Toast.makeText(
@@ -60,6 +76,16 @@ class MainActivity : AppCompatActivity() {
         val ibBrush: ImageButton = findViewById(R.id.ib_brush)
         ibBrush.setOnClickListener{
             showBrushSizeChooserDialog()
+        }
+
+        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
+        ibUndo.setOnClickListener{
+            drawView?.onClickUndo()
+        }
+
+        val ibRedo: ImageButton = findViewById(R.id.ib_redo)
+        ibRedo.setOnClickListener{
+            drawView?.onClickRedo()
         }
 
         val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
