8aba600fd6b82d94033d9ed683a7e0671c2e5c86
WEIKIAT-DESKTOP\Admin <danielgohweikiat@yahoo.com>
2022-06-01 21:01:47 +0800

    add view binding to main activity

--- WorkoutApp/app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ WorkoutApp/app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -4,20 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.workoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener {
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this@MainActivity,
                 "Activity Started!",
                 Toast.LENGTH_SHORT
             ).show()
         }
+
+
+
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+        binding = null
 
     }
 }
