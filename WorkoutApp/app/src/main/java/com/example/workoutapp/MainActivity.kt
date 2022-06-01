20dd2d249493de5b7b60fed9df2cef172d354237
WEIKIAT-DESKTOP\Admin <danielgohweikiat@yahoo.com>
2022-06-01 21:14:26 +0800

    add exercise activity

--- WorkoutApp/app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ WorkoutApp/app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.workoutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -16,11 +17,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding?.root)
 
         binding?.flStart?.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Activity Started!",
-                Toast.LENGTH_SHORT
-            ).show()
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
 
 
