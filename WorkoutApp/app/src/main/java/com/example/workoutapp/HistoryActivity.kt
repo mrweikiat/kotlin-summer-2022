d9885d520487c3b0acd4a0206cd490fa9ec3aa37
WEIKIAT-DESKTOP\Admin <danielgohweikiat@yahoo.com>
2022-06-16 23:19:37 +0800

    add UI element for history activity

---
+++ WorkoutApp/app/src/main/java/com/example/workoutapp/HistoryActivity.kt
@@ -0,0 +1,26 @@
+package com.example.workoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workoutapp.databinding.ActivityHistoryBinding
+
+class HistoryActivity : AppCompatActivity() {
+
+    private var binding: ActivityHistoryBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarHistoryActivity)
+        if(supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = "History"
+        }
+
+        binding?.toolbarHistoryActivity?.setNavigationOnClickListener{
+            onBackPressed()
+        }
+    }
+}
